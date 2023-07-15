36b3f8a172bbdbe67ec259afbb84aca5bf835537
Saleemkhan9741 <saleemkhan9741@gmail.com>
2023-07-15 21:58:08 +0530

    updated code

--- src/test/java/reqres/Test1.java
+++ src/test/java/reqres/Test1.java
@@ -7,19 +7,28 @@ import org.saleem.model.User;
 import org.saleem.restclient.ReqresClient;
 import org.saleem.utils.SerializationHelper;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Test1 extends BaseTest{
 
     private static final Logger LOGGER = LogManager.getLogger(Test1.class);
 
-    @Test
-    public void verifyUserCreation() throws JsonProcessingException {
-        User user = User.builder().name("saleem").job("leader").build();
+    @DataProvider(name = "createUsers")
+    private Object[][] createUsers(){
+        return new Object[][]{
+                {"saleem","leader"},
+                {"pooja","SDET"}
+        };
+    }
+
+    @Test(dataProvider = "createUsers")
+    public void verifyUserCreation(String name, String job) throws JsonProcessingException {
+        User user = User.builder().name(name).job(job).build();
         User createdUser = ReqresClient.getInstance()
                 .createUsers(SerializationHelper.getObjectMapper().writeValueAsString(user));
-        Assert.assertEquals(createdUser.getJob(),"leader");
-        Assert.assertEquals(createdUser.getName(),"saleem");
+        Assert.assertEquals(createdUser.getJob(),job);
+        Assert.assertEquals(createdUser.getName(),name);
     }
 
 }
