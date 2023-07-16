5e0c6892d7139889a4cc7eeb618d2dcf9fc57d40
Saleemkhan9741 <saleemkhan9741@gmail.com>
2023-07-16 09:00:03 +0530

    updated code

--- src/test/java/reqres/ListOfUsersTest.java
+++ src/test/java/reqres/ListOfUsersTest.java
@@ -1,12 +1,21 @@
 package reqres;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.saleem.restclient.ReqresClient;
+import org.testng.ITest;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 public class ListOfUsersTest extends BaseTest{
 
+    private static final Logger LOGGER = LogManager.getLogger(ListOfUsersTest.class);
+
+
     @Test(groups = "smoke")
     public void verifyListOfUsers(){
+        LOGGER.info("Running Test "+ Arrays.stream(ListOfUsersTest.class.getMethods()).findFirst().get().getName());
         ReqresClient.getInstance().getListOfUsers();
     }
 }
